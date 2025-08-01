3ebb22942ae9fb02fe8aad5a4ecbbc87ec1a52d2
BrunoBiluca <b.b.da.costa@gmail.com>
2025-08-01 11:06:36 -0300

    💄 Botão de logout em qualquer página interna de notas

--- src/app/shared/app-header/app-header.ts
+++ src/app/shared/app-header/app-header.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { Logout } from "app/auth/logout/logout";
 
 @Component({
   selector: 'app-header',
-  imports: [],
-  template: ` <p>app-footer works!</p> `,
+  imports: [Logout],
+  template: `
+  <app-logout />
+  `,
   styles: '',
 })
 export class AppHeader {}
