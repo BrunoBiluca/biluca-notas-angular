170fc749fe2d453a4f7e0f02642f49760c036685
BrunoBiluca <b.b.da.costa@gmail.com>
2025-08-21 18:03:00 -0300

    ✨ Limpeza da pesquisa por notas

--- src/app/notes/search/search.ts
+++ src/app/notes/search/search.ts
@@ -1,21 +1,25 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from "@angular/material/button";
+import { MatButtonModule } from '@angular/material/button';
 import { NoteSearch } from '../services/note-search';
 
 @Component({
   selector: 'notes-search',
   imports: [FormsModule, MatIconModule, MatButtonModule],
   templateUrl: './search.html',
-  styleUrl: './search.scss'
+  styleUrl: './search.scss',
 })
 export class Search {
   searchTerm = signal<string>('');
   noteSearch = inject(NoteSearch);
 
-  onChangeSearchTerm() {
-    this.noteSearch.update(this.searchTerm())
+  updateGlobally() {
+    this.noteSearch.update(this.searchTerm());
   }
 
+  clear() {
+    this.searchTerm.set('');
+    this.updateGlobally();
+  }
 }
