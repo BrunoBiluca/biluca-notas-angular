32154cce24c98c9da1706f9b8f480063312dbcab
BrunoBiluca <b.b.da.costa@gmail.com>
2025-09-29 11:32:58 -0300

    ✨ Persistência do tema

--- src/app/shared/theme-selector/theme-selector.ts
+++ src/app/shared/theme-selector/theme-selector.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
@@ -10,7 +10,7 @@ import { MatTooltip } from '@angular/material/tooltip';
   templateUrl: './theme-selector.html',
   styleUrl: './theme-selector.scss',
 })
-export class ThemeSelector {
+export class ThemeSelector implements OnInit {
   themes = [
     {
       theme: 'dark-mode',
@@ -21,7 +21,15 @@ export class ThemeSelector {
     { theme: 'system', label: 'Adaptar ao sistema', icon: 'settings_suggest' },
   ];
 
+  ngOnInit(): void {
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+      this.choose(theme);
+    }
+  }
+
   choose(theme: string) {
+    localStorage.setItem('theme', theme);
     document.documentElement.classList.remove(
       ...this.themes.filter((t) => t.theme !== 'system').map((t) => t.theme)
     );
